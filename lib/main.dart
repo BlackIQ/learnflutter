afabb86b60ee9dc7507da55556fe5e9b215b3ccf
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-10-15 02:04:26 +0330

    Change color and top text

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Learning Flutter.'),
     );
   }
 }
